c186c5668d1aa2878cb602d3234e4f4a42fa4054
TinhTienTi <antoniotinh1998@gmail.com>
2021-01-13 22:31:54 +0700

    all commit

---
+++ BlogSpot/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace BlogSpot.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+    }
+}
